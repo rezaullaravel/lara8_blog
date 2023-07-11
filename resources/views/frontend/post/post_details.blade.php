abbee6647c6bd680731472ea146cee81dfd906aa
[200~rezaullaravel~ <mrkkarim1991@gmail.com>
2023-07-12 01:48:16 +0600

    disqus comment add

--- resources/views/frontend/post/post_details.blade.php
+++ resources/views/frontend/post/post_details.blade.php
@@ -99,99 +99,126 @@ Post Detail Page
                         </div>
                      </div>
                   </div>
-                  @if (count($comments)>0)
-                  <div class="col-lg-12">
-                     <div class="sidebar-item comments">
-                        <div class="sidebar-heading">
-                           <h2>{{count($comments)}} Comments</h2>
-                        </div>
-                        <div class="content">
-                           @foreach ($comments as $comment)
-                           <ul>
-                              <li>
-                                 <div class="right-content">
-                                    <h4>{{$comment['users']['name']}}<span>{{$comment->created_at->format('F j, Y')}}</span></h4>
-                                    <p>{!!$comment->comment_description!!}</p>
-                                    <a href="javascript:void(0)" data-commentId="{{ $comment->id }}"  class="text-info" onclick="showReply(this)">Reply</a>
-                                    @if(Auth::check() && Auth::user()->id==$comment->user_id)
-                                    <a href="{{route('comment.edit',
-                                       ['comment_id'=>$comment->id,
-                                       'post_id'=>$post->id,
-                                       ])}}" class="text-success">Edit</a>
-                                    <a href="{{route('comment.delete',$comment->id)}}" id="delete2" class="text-danger">Delete</a>
-                                    @endif
-                                    <br><br>
-                                    <div>
-                                    {{-- <p><a href="javascript:void(0)" onclick="showLoop(this)">{{ count($comment->replyComments) }} replies</a></p> --}}
-                                    <div class="replyLoop" style="background: #f2f3f4;">
-                                       {{-- comment reply show --}}
-                                       @foreach ($comment->replyComments as $reply)
-                                       <div class="right-content" style="padding:10px;">
-                                          <p>{{ $reply->created_at }}</p>
-                                          <h4>{{ $reply->name }}</h4>
-                                          <p>{{ $reply->description }}</p>
-                                          <a href="javascript:void(0)" data-commentId="{{ $comment->id }}"  class="text-info" onclick="showReply(this)">Reply</a>
-                                       </div>
-                                       @endforeach
-                                       {{-- comment reply show end --}}
-                                    </div>
-                                 </div>
-                        </div>
-                        </li>
-                        </ul>
-                        @endforeach
-                     </div>
-                  </div>
-               </div>
-               @else
-               <div class="col-lg-12">
-                  <h3 class="text-center text-success">No Comments Yet.</h3>
-               </div>
-               @endif
-               {{-- add reply section --}}
-               <div class="col-lg-12 replysection" style="display: none;">
-                  <form action="{{ route('reply.store')}}" method="POST">
-                     @csrf
-                     <input type="hidden" name="commentId" id="commentId">
-                     <fieldset>
-                        <textarea name="description" rows="4"   required="" class="form-control" placeholder="write something here........"></textarea>
-                     </fieldset>
-                     <fieldset>
-                        <button type="submit"  class="btn btn-info" style="cursor: pointer; float:right; margin:3px;">Reply</button>
-                        <button type="button" onclick="closeReply(this)" class="btn btn-primary" style="cursor: pointer; float:right; margin-top:3px;">Close</button>
-                     </fieldset>
-                  </form>
-               </div>
-               {{-- add reply section end --}}
-               {{-- add comment section --}}
-               <div class="col-lg-12">
-                  <div class="sidebar-item submit-comment">
-                     <div class="sidebar-heading">
-                        <h2>Your comment</h2>
-                        @if (Session('sms'))
-                        <h4 class="alert alert-secondary">{{Session::get('sms')}}</h4>
-                        @endif
-                     </div>
-                     <div class="content">
-                        <form  action="{{route('store.comment')}}" method="post">
-                           @csrf
-                           <input type="hidden" name="id" value="{{$post->id}}">
-                           <div class="row">
-                              <div class="col-lg-12">
-                                 <fieldset>
-                                 <textarea name="comment_description" rows="6"  placeholder="Type your comment" required="" ></textarea>
-                              </div>
-                              <div class="col-lg-12">
-                                 <fieldset>
-                                    <button type="submit" id="form-submit" class="main-button" style="cursor: pointer;">Submit</button>
-                                 </fieldset>
-                              </div>
-                           </div>
-                        </form>
-                     </div>
-                  </div>
-               </div>
-               {{-- add comment section end --}}
+
+                 <!-- comment and reply comment section start
+                 @if (count($comments)>0)
+                 <div class="col-lg-12">
+                    <div class="sidebar-item comments">
+                       <div class="sidebar-heading">
+                          <h2>{{count($comments)}} Comments</h2>
+                       </div>
+                       <div class="content">
+                          @foreach ($comments as $comment)
+                          <ul>
+                             <li>
+                                <div class="right-content">
+                                   <h4>{{$comment['users']['name']}}<span>{{$comment->created_at->format('F j, Y')}}</span></h4>
+                                   <p>{!!$comment->comment_description!!}</p>
+                                   <a href="javascript:void(0)" data-commentId="{{ $comment->id }}"  class="text-info" onclick="showReply(this)">Reply</a>
+                                   @if(Auth::check() && Auth::user()->id==$comment->user_id)
+                                   <a href="{{route('comment.edit',
+                                      ['comment_id'=>$comment->id,
+                                      'post_id'=>$post->id,
+                                      ])}}" class="text-success">Edit</a>
+                                   <a href="{{route('comment.delete',$comment->id)}}" id="delete2" class="text-danger">Delete</a>
+                                   @endif
+                                   <br><br>
+                                   <div>
+                                   {{-- <p><a href="javascript:void(0)" onclick="showLoop(this)">{{ count($comment->replyComments) }} replies</a></p> --}}
+                                   <div class="replyLoop" style="background: #f2f3f4;">
+                                      {{-- comment reply show --}}
+                                      @foreach ($comment->replyComments as $reply)
+                                      <div class="right-content" style="padding:10px;">
+                                         <p>{{ $reply->created_at }}</p>
+                                         <h4>{{ $reply->name }}</h4>
+                                         <p>{{ $reply->description }}</p>
+                                         <a href="javascript:void(0)" data-commentId="{{ $comment->id }}"  class="text-info" onclick="showReply(this)">Reply</a>
+                                      </div>
+                                      @endforeach
+                                      {{-- comment reply show end --}}
+                                   </div>
+                                </div>
+                       </div>
+                       </li>
+                       </ul>
+                       @endforeach
+                    </div>
+                 </div>
+              </div>
+              @else
+              <div class="col-lg-12">
+                 <h3 class="text-center text-success">No Comments Yet.</h3>
+              </div>
+              @endif
+              {{-- add reply section --}}
+              <div class="col-lg-12 replysection" style="display: none;">
+                 <form action="{{ route('reply.store')}}" method="POST">
+                    @csrf
+                    <input type="hidden" name="commentId" id="commentId">
+                    <fieldset>
+                       <textarea name="description" rows="4"   required="" class="form-control" placeholder="write something here........"></textarea>
+                    </fieldset>
+                    <fieldset>
+                       <button type="submit"  class="btn btn-info" style="cursor: pointer; float:right; margin:3px;">Reply</button>
+                       <button type="button" onclick="closeReply(this)" class="btn btn-primary" style="cursor: pointer; float:right; margin-top:3px;">Close</button>
+                    </fieldset>
+                 </form>
+              </div>
+              {{-- add reply section end --}}
+              {{-- add comment section --}}
+              <div class="col-lg-12">
+                 <div class="sidebar-item submit-comment">
+                    <div class="sidebar-heading">
+                       <h2>Your comment</h2>
+                       @if (Session('sms'))
+                       <h4 class="alert alert-secondary">{{Session::get('sms')}}</h4>
+                       @endif
+                    </div>
+                    <div class="content">
+                       <form  action="{{route('store.comment')}}" method="post">
+                          @csrf
+                          <input type="hidden" name="id" value="{{$post->id}}">
+                          <div class="row">
+                             <div class="col-lg-12">
+                                <fieldset>
+                                <textarea name="comment_description" rows="6"  placeholder="Type your comment" required="" ></textarea>
+                             </div>
+                             <div class="col-lg-12">
+                                <fieldset>
+                                   <button type="submit" id="form-submit" class="main-button" style="cursor: pointer;">Submit</button>
+                                </fieldset>
+                             </div>
+                          </div>
+                       </form>
+                    </div>
+                 </div>
+              </div>
+               comment and reply comment  section end
+                 ---->
+
+                 {{-- disqus comment system start --}}
+                 <div class="col-lg-12">
+                    <div id="disqus_thread"></div>
+                            <script>
+                                /**
+                                *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+                                *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
+
+                                var disqus_config = function () {
+                                this.page.url = "{{ route('post.single',$post->id) }}";  // Replace PAGE_URL with your page's canonical URL variable
+                                this.page.identifier = "{{ $post->id }}"; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+                                };
+
+                                (function() { // DON'T EDIT BELOW THIS LINE
+                                var d = document, s = d.createElement('script');
+                                s.src = 'https://reza-blog.disqus.com/embed.js';
+                                s.setAttribute('data-timestamp', +new Date());
+                                (d.head || d.body).appendChild(s);
+                                })();
+                            </script>
+                        <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+                 </div>
+                 {{-- disqus comment system end --}}
             </div>
          </div>
       </div>
@@ -219,6 +246,8 @@ Post Detail Page
       </div>
    </div>
    </div>
+
+   <script id="dsq-count-scr" src="//reza-blog.disqus.com/count.js" async></script>
 </section>
 {{---js for sccial media share start--}}
 {{---js for sccial media share end--}}
